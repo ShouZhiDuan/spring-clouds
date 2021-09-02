a3f5a03b3eb2b26b9632a14c75cd69231ffe4d8c
段守志 <dszazhy1314@163.com>
2021-09-02 11:52:02 +0800

    qk fix code

--- ribbon-client/src/main/java/com/ribbon/dsz/RibbonClientApplication.java
+++ ribbon-client/src/main/java/com/ribbon/dsz/RibbonClientApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.client.OkHttp3ClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
@@ -12,7 +13,8 @@ public class RibbonClientApplication {
     @Bean
     @LoadBalanced //用户客户端Ribbon拦截url重构实现负载均衡
     public RestTemplate restTemplate(){
-        return new RestTemplate();
+        OkHttp3ClientHttpRequestFactory factory = new OkHttp3ClientHttpRequestFactory();
+        return new RestTemplate(factory);
     }
 
     public static void main(String[] args) {
